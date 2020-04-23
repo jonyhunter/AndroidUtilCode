e3d8abf861ce00da61a992c471f52920d409fc0c
JonyHunter <jonyhunter@vip.qq.com>
2020-04-23 22:40:08 +0800

    1.删除feature_subutil.

--- feature/main/pkg/src/main/java/com/blankj/main/pkg/MainActivity.kt
+++ feature/main/pkg/src/main/java/com/blankj/main/pkg/MainActivity.kt
@@ -1,20 +1,6 @@
 package com.blankj.main.pkg
 
-import android.graphics.Color
-import android.os.Bundle
-import android.view.View
-import androidx.appcompat.app.ActionBarDrawerToggle
 import com.blankj.common.activity.CommonActivity
-import com.blankj.common.item.CommonItem
-import com.blankj.common.item.CommonItemClick
-import com.blankj.subutil.export.api.SubUtilApi
-import com.blankj.utilcode.export.api.UtilCodeApi
-import com.blankj.utilcode.util.ApiUtils
-import com.blankj.utilcode.util.BarUtils
-import com.blankj.utilcode.util.ClickUtils
-import com.blankj.utilcode.util.CollectionUtils
-import kotlinx.android.synthetic.main.activity_main.*
-
 
 /**
  * ```
@@ -26,49 +12,9 @@ import kotlinx.android.synthetic.main.activity_main.*
  */
 class MainActivity : CommonActivity() {
 
-    override fun isSwipeBack(): Boolean {
-        return false
-    }
-
-    override fun bindDrawer(): Boolean {
-        return true
-    }
-
     override fun bindLayout(): Int {
         return R.layout.activity_main
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        window.setBackgroundDrawable(null)
-        super.onCreate(savedInstanceState)
-    }
 
-    override fun initView(savedInstanceState: Bundle?, contentView: View?) {
-        super.initView(savedInstanceState, contentView)
-        setCommonItems(mainRv, CollectionUtils.newArrayList<CommonItem<*>>(
-                CommonItemClick(R.string.core_util, true) {
-                    ApiUtils.getApi(UtilCodeApi::class.java).startUtilCodeActivity(this)
-                },
-                CommonItemClick(R.string.sub_util, true) {
-                    ApiUtils.getApi(SubUtilApi::class.java).startSubUtilActivity(this)
-                }
-        ))
-
-        launcherMainCtl.setExpandedTitleColor(Color.TRANSPARENT)
-        setSupportActionBar(launcherMainToolbar)
-        val toggle = ActionBarDrawerToggle(this,
-                drawerView.mBaseDrawerRootLayout,
-                launcherMainToolbar,
-                R.string.navigation_drawer_open,
-                R.string.navigation_drawer_close)
-        drawerView.mBaseDrawerRootLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        BarUtils.setStatusBarColor4Drawer(drawerView.mBaseDrawerRootLayout, launcherMainFakeStatusBar, Color.TRANSPARENT, false)
-        BarUtils.addMarginTopEqualStatusBarHeight(launcherMainToolbar)
-    }
-
-    override fun onBackPressed() {
-        ClickUtils.back2HomeFriendly("Press again to exit.")
-    }
 }
